4077cebee1b417778469f67e297438d693d3b8bd
CaidevOficial <falcone.facu@gmail.com>
2022-04-03 16:49:07 -0300

    Mdf ♻️ - App files

--- GameRoom/src/app/app-routing.module.ts
+++ GameRoom/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 // ?# Routes of the application
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
+  {path: 'signup', component: SignupComponent},
   {path: 'about', component: AboutComponent},
 ];
 
--- GameRoom/src/app/app.module.ts
+++ GameRoom/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
