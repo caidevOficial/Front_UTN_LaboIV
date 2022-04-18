b271f79e45640272ebe7ac83f9b6975d9c8e0613
CaidevOficial <falcone.facu@gmail.com>
2022-04-18 02:14:42 -0300

    Upg :recycle: - Time component.

--- GameRoom/src/assets/Time/js_scripts/digital_clock.js
+++ GameRoom/src/assets/Time/js_scripts/digital_clock.js
@@ -22,15 +22,16 @@ setInterval(() => {
     m = (m < 10) ? `0${m}` : m;
     s = (s < 10) ? `0${s}` : s;
 
-    hours.innerHTML = `${h}<br><span>Hours</span>`;
-    minutes.innerHTML = `${m}<br><span>Minutes</span>`;
-    seconds.innerHTML = `${s}<br><span>Seconds</span>`;
+    hours.innerHTML = (h < 2) ? `${h}<br><span>Hour</span>`: `${h}<br><span>Hours</span>`;
+    minutes.innerHTML = (m < 2) ? `${m}<br><span>Minute</span>`: `${m}<br><span>Minutes</span>`;
+    seconds.innerHTML = (s < 2) ? `${s}<br><span>Second</span>`: `${s}<br><span>Seconds</span>`;
     
-    hh.style.strokeDashoffset = 440 - (440 * h) / 25;
-    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
-    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+    hh.style.strokeDashoffset = 440 - ((440 * h) / 24);
+    mm.style.strokeDashoffset = 440 - ((440 * m) / 60);
+    ss.style.strokeDashoffset = 440 - ((440 * s) / 60);
     
-    get_element('.hr_dot').style.transform = `rotate(${(h * 30)}deg)`;
+    // !# Rotate 360 degrees for each hour, minute and second
+    get_element('.hr_dot').style.transform = `rotate(${(h * 15)}deg)`;
     get_element('.min_dot').style.transform = `rotate(${(m * 6)}deg)`;
     get_element('.sec_dot').style.transform = `rotate(${(s * 6)}deg)`;
     
